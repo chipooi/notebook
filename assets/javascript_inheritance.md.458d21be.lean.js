551ee66c9dcc2393fa3ed08b45c5c40decfbc0cc
chipooi <chipooi@163.com>
2021-05-24 20:54:15 +0800

    commit

--- assets/javascript_inheritance.md.458d21be.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as e}from"./app.167eafd2.js";const r='{"title":"","description":"","frontmatter":{},"relativePath":"javascript/inheritance.md","lastUpdated":1621848897151}',a={};a.render=function(r,a,i,n,o,d){return t(),e("div")};export default a;export{r as __pageData};
